875e541a2cadb9ec39e794cf018460ccabb8f522
0mRai <mymail.omnarainrai@gmail.com>
2024-01-22 01:45:29 +0530

    Stack

--- Stack/IamMain.java
+++ Stack/IamMain.java
@@ -1,5 +1,12 @@
-package Stack;
-
 public class IamMain {
-    
+    public static void main(String[] args) {
+        StackCl a = new StackCl(5);
+        a.push(10);
+        a.push(6);
+        a.push(7);
+        a.push(8);
+        System.out.println(a.pop());
+        a.push(11);
+        System.out.println(a.pop());
+    }
 }
--- Stack/StackCl.java
+++ Stack/StackCl.java
@@ -1,5 +1,39 @@
-package Stack;
-
+// there is no dynamic resizing as we are not using arraylist
 public class StackCl {
-    
+    int[] arr;
+    int cap;
+    int top=-1;
+    public StackCl(int c)
+    {
+        cap=c;
+        arr=new int[cap];
+    }
+    void push(int x)
+    {
+        if(top==cap-1)
+        {
+            System.out.println("Stack is full");
+        }
+        else
+        {
+            top++;
+            arr[top]=x;
+        }
+    }
+    int pop()
+    {
+        if(top==-1)
+        {
+            System.out.println("Stack is empty");
+        }
+        else
+        {
+            int res=arr[top];
+            top--;
+            return res;
+        }
+        int res=arr[top];
+        top--;
+        return res;
+    }
 }
